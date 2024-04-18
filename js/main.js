57774ae94399059613f8d96ed13ef3c8ac46a76c
chamithKavinda <chamith13kavinda@gmail.com>
2024-04-18 23:26:13 +0530

    Testimonial section done

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function ($){
+    "use strict";
+
+    //Testimonials
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin:25,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+})(jQuery);
